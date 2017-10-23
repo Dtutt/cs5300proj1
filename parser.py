d4757a2a29183eef86d8b89f2fb72985a4f9a7c0
Luka Ivicevic <ivicevicluka@gmail.com>
2017-10-23 12:41:36 -0500

    adding working tests

--- parser.py
+++ parser.py
@@ -20,7 +20,7 @@ ident = Word(alphas, alphanums + "_$").setName("Identifier")
 columnName = (delimitedList(ident, ".", combine=True)).setName("Column Name").addParseAction(upcaseTokens)
 columnNameList = Group(delimitedList(columnName))
 tableName = (delimitedList(ident, ".", combine=True)).setName("Table Name").addParseAction(upcaseTokens)
-tablenameList = Group(delimitedList(tableName))
+tableNameList = Group(delimitedList(tableName))
 
 whereExpression = Forward()
 and_ = Keyword("and", caseless=True)
@@ -42,48 +42,11 @@ whereCondition = Group(
     (columnName + in_ + "(" + selectStmt + ")") or
     ("(" + whereExpression + ")")
 )
-whereExpression << whereCondition + ZeroOrMore((and_ or or_) + whereExpression)
+whereExpression = whereCondition + ZeroOrMore((and_ or or_) + whereExpression)
 
 # Define the SQL grammar
-selectStmt <<= (SELECT + ('*' or columnNameList)("columns") + FROM + tableNameList("tables") + Optional(Group(WHERE + whereExpression), "")("where"))
-sql = selectStmt
+selectStmt = SELECT + ('*' or columnNameList) + FROM + tableNameList("tables") + Optional(Group(WHERE + whereExpression))
 
 # Run tests
-if __name__ == "__main__":
-    sql.runTests("""\
-
-        # multiple tables
-        SELECT * from XYZZY, ABC
-
-        # dotted table name
-        select * from SYS.XYZZY
-
-        Select A from Sys.dual
-
-        Select A,B,C from Sys.dual
-
-        Select A, B, C from Sys.dual, Table2   
-
-        # FAIL - invalid SELECT keyword
-        Xelect A, B, C from Sys.dual
-
-        # FAIL - invalid FROM keyword
-        Select A, B, C frox Sys.dual
-
-        # FAIL - incomplete statement
-        Select
-
-        # FAIL - incomplete statement
-        Select * from
-
-        # FAIL - invalid column
-        Select &&& frox Sys.dual
-
-        # where clause
-        Select A from Sys.dual where a in ('RED','GREEN','BLUE')
-
-        # compound where clause
-        Select A from Sys.dual where a in ('RED','GREEN','BLUE') and b in (10,20,30)
-
-        # where clause with comparison operator
-        Select A,b from table1,table2 where table1.id eq table2.id""")
+test = "SELECT * from XYZZY, ABC"
+print(test, "->", selectStmt.parseString(test))
