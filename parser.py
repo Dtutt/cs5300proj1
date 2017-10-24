d5cbeb6dff8ba0271ec524e0bc49dcd5a45e2c4e
Luka Ivicevic <ivicevicluka@gmail.com>
2017-10-24 13:01:47 -0500

    working on parser validation

--- parser.py
+++ parser.py
@@ -6,7 +6,6 @@
 #       it is unicode apostrophe.
 # 
 #
-#
 
 # Imports
 from pyparsing import Literal, CaselessLiteral, Word, delimitedList, Optional, \
@@ -72,35 +71,72 @@ SQLParser = selectStmt # TODO - make paranthesies optional around a selectStmt (
 
 # Tests
 if __name__ == "__main__":
-    test = "SELECT count(D), T, max(R), E FROM Sailors as T, Boats WHERE count(r) = 2 and S2.rating = 10 GROUP BY D.sdf Having count(d) > 5 or max(R) = 5"
+    test = "SELECT sid, sname as s, max(rating), E FROM Sailors as T, Boats WHERE count(r) = 2 and S2.rating = 10 GROUP BY D.sdf Having count(d) > 5 or max(R) = 5"
     try:
-        print(test, "\n-----\n", SQLParser.parseString(test))
+        #print(test, "\n-----\n", SQLParser.parseString(test))
         parsedQuery = SQLParser.parseString(test)
     except Exception as e:
         print("Syntax Error parsing: " + test)
         print(e)
     
-    # attributes: list of attributes and their type (comes after select)
-    attributes = parsedQuery[1]
-    print(attributes)
     # List of tables being used
     tables = parsedQuery[3]
-    print(tables)
+    # attributes: list of attributes and their type (comes after select)
+    attributes = parsedQuery[1]
 
     # Define the schema
     sailors = (
+        ("tname", "sailors"),
         ("sid", "int"),
         ("sname", "str"),
         ("rating", "int"),
         ("age", "real")
     )
     boats = (
+        ("tname", "boats"),
         ("bid", "int"),
         ("bname", "str"),
         ("color", "str")
     )
     reserves = (
+        ("tname", "reserves"),
         ("sid", "int"),
         ("bid", "int"),
         ("day", "date")
     )
+
+    # Check if the tables and attributes are valid according to the schema
+    for item in tables:
+        if (str(item[0]).upper() != sailors[0][1].upper()) and (str(item[0]).upper() != reserves[0][1].upper()) and (str(item[0]).upper() != boats[0][1].upper()):
+            print(item[0] + " is not a table in the schema.")
+    
+    # Check if the select attributes are valid according to the schema and what tables are being used in the query
+    print(attributes)
+    # iterate through each attributes
+    # Check if it's a built-in function, if it is then get the 2 index (that will be the attribute)
+    # If it's not a build in function, then get the 0 index (that will be the attribute)
+    # Check if that attribute is in any of the tables
+    # If it is, make sure that table is being used in the query (check if the table is in 'tables')
+    for attribute in attributes:
+        # Extract the correct attribute
+        if (str(attribute[0]).upper() == "COUNT") or (str(attribute[0]).upper() == "MAX") or (str(attribute[0]).upper() == "AVG") or (str(attribute[0]).upper() == "SUM"):
+            attr = attribute[2]
+        else:
+            attr = attribute[0]
+        # Check if the attribute is in any of the tables in the schema
+        isInTable = False
+        for item in sailors:
+            if (item[0].upper() == attr):
+                isInTable = True
+                break
+        for item in boats:
+            if (item[0].upper() == attr):
+                isInTable = True
+                break
+        for item in reserves:
+            if (item[0].upper() == attr):
+                isInTable = True
+                break
+        if (isInTable == False):
+            print(attr + " is not an attribute in the schema.")
+        print(attr + str(isInTable))
