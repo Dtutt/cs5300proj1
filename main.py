305798673be7ededb2be80b4e8b4b51fc1243c8f
Dtutt95 <dtutt95@gmail.com>
2017-10-25 20:03:44 -0500

    RA translation

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+# CS 5300 Project 1 SQL -> relational algebra
+# Luka Ivicevic and David Tutt
+# Notes:
+#   -   Trouble matching some single quoted strings, if test input is copy and pasted from website,
+#       the single quotes used are unicode right/left single quotation mark, if typed from my keyboard,
+#       it is unicode apostrophe.
+#
+#
 
-import parser
-loop=1
+# Imports
+import sqlparser
+import SQLRA
+loop=True
 
 
 
 while loop:
     print("1. Run Query")
     print("2. End")
-    choice = input("enter number of option")
+    #choice = input("enter number of option")
+    choice='1'
     if choice=='1':
-        SQL=input("Enter SQL query")
-        SQLparse=parser(SQL)
-        RAstr=SQL_RA(SQLparse)
-        tree=RA_Tree(RAstr)
+        #SQL=input("Enter SQL query")
+        SQL="SELECT sid, sname as s, max(rating), E FROM Sailors as T, Boats WHERE count(sid) = 2 and S2.rating = 10 and sid = 4 GROUP BY sid Having count(d) > 5 or max(R) = 5"
+        RAstr=sqlparser.sqlparse(SQL)
+        #tree=RA_Tree(RAstr)
+        loop=False
     elif choice=='2':
-        loop=0
+        loop=False
 
 
